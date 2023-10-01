56d4061921d4354c7fded21e375704d6a034c852
Vitalii Vyrodov <gtopbox@gmail.com>
2023-10-01 09:12:58 +0200

    Adds memory store

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -6,16 +6,17 @@ class FakeAuthRepository {
 
   AppUser? get currentUser => null;
 
-  Future<void> signInWithEmailAndPassword() async {
-    await Future.delayed(Duration(seconds: 1));
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    await Future.delayed(const Duration(seconds: 1));
   }
 
-  Future<void> createUserWithEmailAndPassword() async {
-    await Future.delayed(Duration(seconds: 1));
+  Future<void> createUserWithEmailAndPassword(
+      String email, String password) async {
+    await Future.delayed(const Duration(seconds: 1));
   }
 
   Future<void> signOut() async {
-    await Future.delayed(Duration(seconds: 1));
+    await Future.delayed(const Duration(seconds: 1));
   }
 }
 
