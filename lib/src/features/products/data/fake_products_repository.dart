bfe52c8391de07b4323418d2f35a8bb0cc68c1f3
Vitalii Vyrodov <gtopbox@gmail.com>
2023-10-01 07:38:09 +0200

    Init

---
+++ lib/src/features/products/data/fake_products_repository.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+
+import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FakeProductsRepository {
+  final List<Product> _products = kTestProducts;
+
+  List<Product> getProductsList() {
+    return _products;
+  }
+
+  Product? getProduct(String id) {
+    return _products.firstWhere((product) => product.id == id);
+  }
+
+  Future<List<Product>> fetchProductsList() async {
+    await Future.delayed(const Duration(seconds: 2));
+    //throw Exception('Error fetching products');
+    return Future.value(_products);
+  }
+
+  Stream<List<Product>> watchProductsList() async* {
+    await Future.delayed(const Duration(seconds: 2));
+
+    yield _products;
+  }
+
+  Stream<Product?> watchProduct(String id) {
+    return watchProductsList()
+        .map((products) => products.firstWhere((product) => product.id == id));
+  }
+}
+
+final productsRepositoryProvider = Provider<FakeProductsRepository>((ref) {
+  return FakeProductsRepository();
+});
+
+final productsListStreamProvider =
+    StreamProvider.autoDispose<List<Product>>((ref) {
+  final productsRepository = ref.watch(productsRepositoryProvider);
+  return productsRepository.watchProductsList();
+});
+
+final productsListFutureProvider =
+    FutureProvider.autoDispose<List<Product>>((ref) {
+  final productsRepository = ref.watch(productsRepositoryProvider);
+  return productsRepository.fetchProductsList();
+});
+
+final productProvider =
+    StreamProvider.autoDispose.family<Product?, String>((ref, id) {
+  debugPrint('productProvider $id created');
+  ref.onDispose(() => debugPrint('productProvider $id disposed'));
+  var link = ref.keepAlive();
+  Timer(const Duration(seconds: 10), link.close);
+  final productsRepository = ref.watch(productsRepositoryProvider);
+  return productsRepository.watchProduct(id);
+});
