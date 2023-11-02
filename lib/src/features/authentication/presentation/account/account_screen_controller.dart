8d40c7a3d619d94428a7dc9a9b37650604022200
Vitalii Vyrodov <gtopbox@gmail.com>
2023-11-02 08:31:54 +0100

    Adding AsyncValue extension

--- lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -1,9 +1,9 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AccountScreenController extends StateNotifier<AsyncValue<void>> {
+class AccountScreenController extends StateNotifier<AsyncValue> {
   AccountScreenController({required this.authRepository})
-      : super(const AsyncValue<void>.data(null));
+      : super(const AsyncValue.data(null));
   final FakeAuthRepository authRepository;
 
   Future<bool> signOut() async {
@@ -14,7 +14,7 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>> {
 }
 
 final accountScreenControllerProvider = StateNotifierProvider.autoDispose<
-    AccountScreenController, AsyncValue<void>>((ref) {
+    AccountScreenController, AsyncValue>((ref) {
   final authRepository = ref.watch(authRepositoryProvider);
   return AccountScreenController(authRepository: authRepository);
 });
